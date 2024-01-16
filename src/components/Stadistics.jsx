a5d1dcee0089f14b6f2a1d6da1dbf8120726dbc4
FernandoDD01 <fealvarado07@gmail.com>
2024-01-15 21:50:50 -0600

    Prototipo de grafica

--- src/components/Stadistics.jsx
+++ src/components/Stadistics.jsx
@@ -2,11 +2,14 @@ import { useContext } from "react";
 import Dashboard from "./Dashboard";
 import Folders from "./Folders";
 import { ThemeContext } from "../context/themeContext";
+import GraphicCircle from "./GraphicCircle";
 
 export default function Stadistics() {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <main className={`${theme.darkmode ? "dark" : "light"}`}>Estadísticas</main>
+    <main className={`${theme.darkmode ? "dark" : "light"}`}>
+      <GraphicCircle></GraphicCircle>
+    </main>
   );
 }
